56a40e20539731807e6669427c68fa0497c23b42
Mike Alhayek <mike@crestapps.com>
2017-09-28 10:53:27 -0700

    Fixes resource-file command issue

--- src/Commands/ResourceFileDeleteCommand.php
+++ src/Commands/ResourceFileDeleteCommand.php
@@ -3,9 +3,9 @@
 namespace CrestApps\CodeGenerator\Commands;
 
 use CrestApps\CodeGenerator\Commands\Bases\ResourceFileCommandBase;
-use CrestApps\CodeGenerator\Models\ResourceMapper;
 use CrestApps\CodeGenerator\Support\Config;
 use CrestApps\CodeGenerator\Support\Helpers;
+use CrestApps\CodeGenerator\Support\ResourceMapper;
 
 class ResourceFileDeleteCommand extends ResourceFileCommandBase
 {
