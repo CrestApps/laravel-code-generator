eb86495bc3c593fb3a182fd21835e0d9e670ea26
Mike Alhayek <mike@crestapps.com>
2024-01-27 15:44:35 -0800

    fix edit url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/CrestApps/laravel-code-generator/docs/docs/edit/master/',
+            'https://github.com/CrestApps/laravel-code-generator/edit/master/docs/',
         },
         theme: {
           customCss: './src/css/custom.css',
