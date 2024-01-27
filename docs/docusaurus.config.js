5fbc8745940aa4cd5c0d41068ae3bb8c3634985e
Mike Alhayek <mike@crestapps.com>
2024-01-27 13:03:22 -0800

    fix doc links

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://www.crestapps.com',
+  url: 'https://laravel-code-generator.crestapps.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
